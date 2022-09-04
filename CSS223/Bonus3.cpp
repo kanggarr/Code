e9d2c178e54e7e4008adbbc261b727b502d2889f
Pumipat <porpumipat@gmail.com>
2022-09-04 19:58:40 +0700

    main

--- CSS223/Bonus3.cpp
+++ CSS223/Bonus3.cpp
@@ -1,14 +1,9 @@
+// Scoreboard 10 students Using Doubly Linked List when add newscore to scoreboard, it will be added to the right position and the lowest score will be removed.
+
 #include <iostream>
 
 using namespace std;
 
-struct students
-{
-    string name;
-    int score;
-} student[10];
-
-int main()
-{
+int main(){
     
 }
