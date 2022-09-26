b26eb9994806a1cebfa5e21f3a8eaa54783e663a
Pumipat <porpumipat@gmail.com>
2022-09-26 10:27:45 +0700

    master

--- CSS222/inheritance/inheritance/src/HourlyWorker.java
+++ CSS222/inheritance/inheritance/src/HourlyWorker.java
@@ -1,11 +1,15 @@
 public class HourlyWorker extends Worker {
+
     private int hours_Worked;
+
     HourlyWorker(String name, double salary_rate, int hours_Worked){
         super(name, salary_rate);
         this.hours_Worked = hours_Worked;
     }    
+
     @Override
     public double computePay() {
         return 50*hours_Worked;
     }
+
 }
